2688e91d509ed834010807327b3e58894468c257
6yiyiliang <1583342630@qq.com>
2024-07-16 21:27:01 +0800

    init:初始化项目

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/Curriculum-Vitae/",
 
   lang: "zh-CN",
   title: "文档演示",
