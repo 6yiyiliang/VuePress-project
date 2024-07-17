7c30794f1d25abc2def1f75729def5540396253d
6yiyiliang <1583342630@qq.com>
2024-07-17 18:29:20 +0800

    base更改

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/Curriculum-Vitae/",
+  base: "/VuePress-project/",
 
   lang: "zh-CN",
   title: "首页",
